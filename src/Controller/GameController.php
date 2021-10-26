94f330becf4016e8ac5d6419cf6428855d9c03fe
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-10-26 16:06:50 +0200

    add twig

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -7,23 +7,32 @@ use App\Model\GameModel;
 class GameController
 {
     private GameModel $model;
+    private \Twig\Environment $twig;
 
     public function __construct()
     {
         $this->model = new GameModel();
+
+        $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../View');
+        $this->twig = new \Twig\Environment(
+            $loader,
+            [
+                'cache' => false,
+            ]
+        );
     }
 
     public function browse(): void
     {
         $games = $this->model->getAll();
 
-        require_once __DIR__ . '/../View/games/browse.php';
+        echo $this->twig->render('games/browse.html.twig', ['games' => $games]);
     }
 
     public function show(int $id): void
     {
         $game = $this->model->getById($id);
 
-        require_once __DIR__ . '/../View/games/show.php';
+        echo $this->twig->render('games/show.html.twig', ['game' => $game]);
     }
 }
